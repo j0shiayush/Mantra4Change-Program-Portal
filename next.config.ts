9cacccd61bac714bf0e0c17bccc38ab97ba9344d
j0shiayush <aj326108@gmail.com>
2026-06-25 13:39:18 +0530

    fix: update next.config.ts to trace sqlite database

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // This forces Vercel to include Prisma and your SQLite file in the serverless functions
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
